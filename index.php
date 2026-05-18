21df49e6789828e3262bbb57ea85bb8beb207e40
フオリ <176538447+brvnotrocoli@users.noreply.github.com>
2026-05-18 20:38:49 -0300

    v0.6

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 <html>
     <head>
         <title>[ biosystem ]</title>
-        
+
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        
+
         <link rel="stylesheet" href="assets/ui.css">
         <link rel="icon shortcut" href="favicon.ico" type="image/x-icon"/>
         <script src="assets/jquery@1.12.4.min.js"></script>
     </head>
-    
+
     <body class="aem">
         <header>
             <img src="https://upload.wikimedia.org/wikipedia/en/5/5d/Thermo_Fisher_Scientific_Logo.svg" class="logo">
@@ -28,9 +28,8 @@
         </main>
 
         <footer>
-            <small>
-                © 2026 - Thermo Fisher Scientific | All trademarks are the property of Thermo Fisher Scientific
-            </small>
+            <p>© Thermo Fisher Scientific | All trademarks are the property of Thermo Fisher Scientific</p>
+            <a href="https://github.com/brvnotrocoli/biosystem" target="_blank">github.com/brvnotrocoli/biosystem</a>
         </footer>
     </body>
 </html>
